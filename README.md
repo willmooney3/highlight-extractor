7c30a8fd1f3f5824d7a92ac576607d2c3a5c7492
Will Mooney <will.mooney.3@gmail.com>
2021-08-22 10:10:03 -0400

    Added initial sloppy version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # highlight-extractor
-A quick script to extract the highlights I make on PDF files using a Kobo Elipsa.
+A quick script to extract the highlights I make on PDF files using a Kobo
+Elipsa.
+
+# Description
+The Kobo Elipsa allows you to export highlights made in documents, as long as
+those highlights are *not* made using the stylus that comes included with the
+eReader. Given that reading and highlighting PDFs was the entire point of me
+buying the thing, I decided to make sure my freehand highlights could be
+extracted from all of the papers I read with some Python.
+
+# Usage
+ 1. Connect your Elipsa to your computer
+ 1. Copy the mounted drive into a folder
+ 1. Plop this script in the new folder
+ 1. Create a virtualenv and install the requirements
+ 1. Run the script with `python highlight_extractor.py`
+
+# Output
+This will create an `annotations` directory, with sub-directories for each file
+that a highlight is detected in. Each file directory is then divided by page,
+with those files containing PNG clips of the highlights. There is an
+`index.html` file that contains the document titles followed by all of the
+embedded image highlights.
---
+++ highlight_extractor.py
@@ -0,0 +1,46 @@
+from glob import iglob
+import pathlib
+
+import fitz
+
+annotations = {}
+for pdf in iglob("*.pdf"):
+    with fitz.open(pdf) as doc:
+        if doc.has_annots():
+            pathlib.Path(f"annotations/{pdf}").mkdir(parents=True, exist_ok=True)
+            annotations[pdf] = []
+            for page in doc.pages():
+                if len(page.get_drawings()):
+                    for drawing in page.get_drawings():
+                        # Try to skip over any drawings that aren't Kobo highlights
+                        if drawing['opacity'] > 0.5:
+                            continue
+                        pathlib.Path(f"annotations/{pdf}/{page.number}").mkdir(parents=True, exist_ok=True)
+                        tl_x = drawing['rect'].tl.x
+                        tl_y = drawing['rect'].tl.y
+                        br_x = drawing['rect'].br.x
+                        br_y = drawing['rect'].br.y
+                        annotations[pdf].append({
+                            "page": page.number,
+                            "topLeft": (tl_x, tl_y),
+                            "bottomRight": (br_x, br_y),
+                        })
+                        # Calculate new rect: Need to add space around to ensure all text is fully
+                        # captured
+                        new_rect = fitz.Rect(
+                            fitz.Point(tl_x - 5, tl_y - 5),
+                            fitz.Point(br_x + 5, br_y + 5)
+                        )
+                        highlight = page.get_pixmap(clip=new_rect)
+                        highlight.save(f"annotations/{pdf}/{page.number}/{tl_x}_{tl_y}_{br_x}_{br_y}.png")
+if annotations:
+    with open("annotations/index.html", "w") as annotation_index:
+        annotation_index.write("<!doctype html>\n<html>\n<head>Annotation Index</head><body>\n")
+        annotation_index.write("<ul>\n")
+        for doc, annots in annotations.items():
+            annotation_index.write(f"<li>{doc}</li>\n<ul>\n")
+            for highlight in annots:
+                annotation_index.write(f"<li><img src=\"{doc}/{highlight['page']}/{highlight['topLeft'][0]}_{highlight['topLeft'][1]}_{highlight['bottomRight'][0]}_{highlight['bottomRight'][1]}.png\"></li>")
+            annotation_index.write("</ul>\n")
+        annotation_index.write("</ul>\n")
+        annotation_index.write("</body></html>")
